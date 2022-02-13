29e88cabf295a005e9e58aad3cceb240cad55069
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-02-14 00:47:06 +0100

    updates for tagging 0.2.0

--- cpp/build.sh
+++ cpp/build.sh
@@ -82,12 +82,21 @@ function get_required_private_repo_github () {
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.25.1" 8
+get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.25.3" 8
 get_required_kitsune_lib_repo "libKitsunemimiJson" "v0.11.3" 1
+get_required_kitsune_lib_repo "libKitsunemimiNetwork" "v0.8.2" 8
 echo ""
 echo "###########################################################################################################"
 echo ""
 get_required_private_repo_github "libKitsunemimiCrypto" "v0.2.0" 8
+echo ""
+echo "###########################################################################################################"
+echo ""
+get_required_kitsune_lib_repo "libKitsunemimiSakuraNetwork" "v0.8.4" 8
+echo ""
+echo "###########################################################################################################"
+echo ""
+get_required_private_repo_gitlab "libKitsunemimiHanamiCommon" "v0.1.0" "2ue6RNxkCDs2A7qp1xtN" 8
 echo ""
 echo "###########################################################################################################"
 
