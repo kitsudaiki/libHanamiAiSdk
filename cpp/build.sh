e60e91e9e14d87dc43ec6b6a6a0a718ad346436a
Tobias Anker <tobias.anker@kitsunemimi.moe>
2022-06-28 17:09:12 +0200

    updates for tagging 0.3.0

--- cpp/build.sh
+++ cpp/build.sh
@@ -82,7 +82,7 @@ function get_required_private_repo_github () {
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.25.3" 8
+get_required_kitsune_lib_repo "libKitsunemimiCommon" "v0.26.0" 8
 get_required_kitsune_lib_repo "libKitsunemimiJson" "v0.11.3" 1
 echo ""
 echo "###########################################################################################################"
@@ -91,7 +91,7 @@ get_required_private_repo_github "libKitsunemimiCrypto" "v0.2.0" 8
 echo ""
 echo "###########################################################################################################"
 echo ""
-get_required_private_repo_gitlab "libKitsunemimiHanamiCommon" "v0.1.0" "2ue6RNxkCDs2A7qp1xtN" 8
+get_required_private_repo_gitlab "libKitsunemimiHanamiCommon" "v0.2.0" "2ue6RNxkCDs2A7qp1xtN" 8
 echo ""
 echo "###########################################################################################################"
 
